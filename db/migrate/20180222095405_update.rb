60efb1e06765d48d234d66ead8c7b93731d3a383
jdevine82 <jdevine82@gmail.com>
2018-02-22 14:45:25 +0000

    stil working on db migrations

--- db/migrate/20180222095405_update.rb
+++ db/migrate/20180222095405_update.rb
@@ -1,5 +1,6 @@
 class CreateOrders< ActiveRecord::Migration
   def change 
+     create_table :orders
       add_column :orders, :date, :datetime
       add_column :orders, :customer_id, :integer
       add_column :orders, :customer_order_no, :string
