a68e7aa5acb66a99c13a5f57ea2394ce021aef59
jdevine82 <jdevine82@gmail.com>
2018-03-01 02:14:24 +0000

    trying to getcoverflow to work

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -3,3 +3,29 @@
       $('.datepicker').datepicker( { dateFormat:'dd-mm-yy' } );
     });
  
+ $(document).ready(function () {
+    //initialize swiper when document ready
+  var swiper = new Swiper('.swiper-container', {
+      effect: 'coverflow',
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: 'auto',
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows : true,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      autoplay: {
+    delay: 5000,
+  },
+   navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+});
