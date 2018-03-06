7c68b67834f54f5cac17e39465b7ac52a187710b
jdevine82 <jdevine82@gmail.com>
2018-03-06 03:13:46 +0000

    added about us

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,4 +10,8 @@ class StaticPagesController < ApplicationController
     @latest_books = Product.order(created_at: :desc).where("created_at >= :start_date AND created_at <= :end_date", {start_date: Date.today<<(6), end_date: Date.today+1.day}).limit(5)
     @author_books = Product.distinct.pluck(:author).first
   end
+  
+  def about
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
+  get 'static_pages/about'
 
 devise_for :users, :controllers => { registrations: 'registrations', sessions: 'users/sessions' }
 
